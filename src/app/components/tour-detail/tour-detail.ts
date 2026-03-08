ff464992b18eccdb5d978e27b5dff48d3954560c
Velichka Georgieva <145003407+vili-georgieva@users.noreply.github.com>
2026-03-08 21:15:32 +0100

    add tour log list component with basic functionality

--- src/app/components/tour-detail/tour-detail.ts
+++ src/app/components/tour-detail/tour-detail.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Tour } from '../../models/tour.model';
+import { TourLogList } from '../tour-log-list/tour-log-list';
 
 @Component({
   selector: 'app-tour-detail',
-  imports: [],
+  imports: [TourLogList],
   templateUrl: './tour-detail.html',
   styleUrl: './tour-detail.css',
 })
